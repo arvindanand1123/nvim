efe81e930b3537125f9650d9eb344e55b037e925
arvindanand1123 <arvindanand1123@gmail.com>
2024-10-10 19:40:16 -0400

    Fixed lualine

--- init.lua
+++ init.lua
@@ -406,11 +406,6 @@ require('lazy').setup({
     config = function()
       require('mini.ai').setup { n_lines = 500 }
       require('mini.surround').setup()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
     end,
   },
   { -- Highlight, edit, and navigate code
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,9 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  priority = 900,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {}
+    end,
+  },
 }
