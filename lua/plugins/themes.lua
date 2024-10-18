50ae3c205841c1ccc23004f2dfa1afb04e1d728a
arvindanand1123 <arvindanand1123@gmail.com>
2024-10-18 16:51:18 -0400

    Updated theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,9 +1,9 @@
 return {
   {
-    'EdenEast/nightfox.nvim',
+    'rafi/awesome-vim-colorschemes',
     priority = 1000,
     init = function()
-      vim.cmd 'colorscheme carbonfox'
+      vim.cmd 'colorscheme purify'
     end,
   },
 }
