476e260a24a0ea6fea9778be06fb5e5c889c4b83
arvindanand1123 <arvindanand1123@gmail.com>
2024-10-31 16:52:43 -0700

    Revert

--- init.lua
+++ init.lua
@@ -372,7 +372,7 @@ require('lazy').setup({
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Enter>'] = cmp.mapping.confirm { select = true },
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
           ['<C-Space>'] = cmp.mapping.complete {},
           ['<C-l>'] = cmp.mapping(function()
             if luasnip.expand_or_locally_jumpable() then
