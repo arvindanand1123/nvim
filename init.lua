00d91b7fd012e513126757f6b50b818a6dd1d4f9
arvindanand1123 <arvindanand1123@gmail.com>
2025-01-23 12:45:57 -0800

    Cleanup + remove DAP plugin
    Use terminal debuggers more

--- init.lua
+++ init.lua
@@ -59,66 +59,17 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Live substitution preview (inccommand)
 vim.opt.inccommand = 'split'
 
--- Quick vertical split with leader v
-vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = 'Create vertical split' })
-
--- Easy way to close current split
-vim.keymap.set('n', '<leader>x', ':close<CR>', { desc = 'Close current split' })
-
--- Make current split full screen
-vim.keymap.set('n', '<leader>z', ':only<CR>', { desc = 'Zoom split (make full screen)' })
-
--- Balance all splits
-vim.keymap.set('n', '<leader>=', '<C-w>=', { desc = 'Make splits equal size' })
-
 -- Highlight current line (cursorline)
 vim.opt.cursorline = true
 
 -- Keep 10 lines visible above/below cursor (scrolloff)
 vim.opt.scrolloff = 10
 
--- [[ Keymaps ]]
--- See `:help vim.keymap.set()`
-
--- Clear search highlights with <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Copy current file path to clipboard
-vim.keymap.set('n', '<leader>cp', function()
-  local filepath = vim.fn.expand '%:p'
-  vim.fn.setreg('+', filepath)
-  print('Copied file path: ' .. filepath)
-end, { desc = 'Copy current file [P]ath' })
-
--- Map <Esc><Esc> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- Uncomment to disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Remap Ctrl+hjkl for window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to left window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to upper window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to right window' })
+-- [[ Basic Editor Keymaps ]]
+require 'base-keymaps'
 
 -- [[ Autocommands ]]
--- See `:help lua-guide-autocommands`
-
--- Highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim
