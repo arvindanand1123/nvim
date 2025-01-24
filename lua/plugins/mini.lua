04733e569bcdd0a36565ce96f414309209b98cdf
arvindanand1123 <arvindanand1123@gmail.com>
2025-01-24 13:52:20 -0800

    Added surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,10 @@
+default_n_lines = 500
 return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
+      require('mini.ai').setup { n_lines = default_n_lines }
+      require('mini.surround').setup { n_lines = default_n_lines }
     end,
   },
 }
