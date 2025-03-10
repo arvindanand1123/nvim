e70d4c15891d9f3301fbdee76f089f7995c51ba1
arvindanand1123 <arvindanand1123@gmail.com>
2025-03-10 14:56:28 -0700

    Lualine denoising

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,28 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      require('lualine').setup {}
+      require('lualine').setup {
+        options = {
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'filename' },
+          lualine_c = { 'branch' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = { 'filename' },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
     end,
   },
 }
